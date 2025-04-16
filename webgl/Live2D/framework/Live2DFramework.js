ad96f07c3a60a2853787d81884d7e3979ac1dcee
NileDev1 <77495892+1jammer1@users.noreply.github.com>
2025-04-16 13:02:19 -0400

    minor spelling error

--- webgl/Live2D/framework/Live2DFramework.js
+++ webgl/Live2D/framework/Live2DFramework.js
@@ -1,7 +1,7 @@
 /**
  *
  *  You can modify and use this source freely
- *  only for the development of application related Live2D.
+ *  only for the development of applications related to Live2D.
  *
  *  (c) Live2D Inc. All rights reserved.
  */
